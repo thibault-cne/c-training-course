c1d51c6e8b42a44f9406d66c07f6382e917d29a5
Thib <thibault.cheneviere@telecomnancy.net>
2022-10-30 13:38:34 +0100

    [+] test sub-exercise 3 workflow

---
+++ Exercise2/includes/dayofbirth.h
@@ -0,0 +1,9 @@
+#ifndef SOLUTION_DAYOFBIRTH
+#define SOLUTION_DAYOFBIRTH
+
+#include <math.h>
+
+int dayfromdate(int day, int month, int year);
+int checkDate(int day, int month, int year);
+
+#endif // SOLUTION_DAYOFBIRTH
